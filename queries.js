8ca178775ffcc58aaee6ab93e31d9f811bb1491d
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-02-15 11:40:10 -0700

    add function for sort irises by class, add restricted psql role

---
+++ queries.js
@@ -0,0 +1,33 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'iris_role',
+  host: 'localhost',
+  database: 'irisapp',
+  password: 'password',
+  port: 5432,
+})
+
+const getIrises = (request, response) => {
+  pool.query('SELECT * FROM irises ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getIrisesByClass = (request, response) => {
+  pool.query(`SELECT * FROM irises WHERE class=$1 ORDER BY id ASC`,
+              [request.params.class],
+                (error, results) => {
+                  if (error) {
+                    throw error
+                  }
+                  response.status(200).json(results.rows)
+                })
+}
+
+module.exports = {
+  getIrises,
+  getIrisesByClass,
+}
