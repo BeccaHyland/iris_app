80f4b8ea9d96b88b87fc616ab15ad8f3188bf52b
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-02-15 12:32:52 -0700

    complete functional differentiated search routes / function - needs input guards

--- index.js
+++ index.js
@@ -16,8 +16,16 @@ app.get('/', (request, response) => {
 })
 
 app.get('/irises', db.getIrises)
+
 app.get('/search/irises/class/:class', db.getIrisesByClass)
+app.get('/search/irises/class', (request, response) => {
+  response.json( { message: `Enter a class name after /search/irises/class/[name here]`})
+})
+
 app.get('/search/irises/location/:location', db.getIrisesByLocation)
+app.get('/search/irises/location', (request, response) => {
+  response.json( { message: `Enter the integer of the location after /search/irises/location/[integer here]`})
+})
 
 
 app.listen(port, () => {
