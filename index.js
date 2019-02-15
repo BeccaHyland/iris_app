471736be4cdd5e71620a9a46f09dce5dec49150b
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-02-15 11:57:37 -0700

    standardize search url tree in anticipation of future non-iris searches

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/', (request, response) => {
 })
 
 app.get('/irises', db.getIrises)
-app.get('/irises/search/class/:class', db.getIrisesByClass)
-app.get('/irises/search/location/:location', db.getIrisesByLocation)
+app.get('/search/irises/class/:class', db.getIrisesByClass)
+app.get('/search/irises/location/:location', db.getIrisesByLocation)
 
 
 app.listen(port, () => {
